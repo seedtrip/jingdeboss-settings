f5324c03fa32abe8f52d60a5b291d96c3c453f56
henryleu <henryleu@126.com>
2015-01-22 18:56:12 +0800

    add redis cnfig

--- index.js
+++ index.js
@@ -22,6 +22,17 @@ var settings = module.exports = {
     level: 'INFO'
   },
 
+  redis:{
+    mode:'single',
+    host: 'localhost',
+    port: 6379,
+    auth:'',
+    sentinel: {
+      hosts:[{host: '127.0.0.1', port: 26379}],
+      masterName:'mymaster'
+    }
+  },
+
   mongo: {
     host: "localhost",
     port: 27017,
